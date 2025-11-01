2eadbc161382e0c22c6f4180d6c38657b35ae3cf
Nasim Ahmad <khannasimahmad@gmail.com>
2025-11-01 11:32:23 +0530

    removed old js files and installed dependencies

--- model/ProcessingState.js
+++
@@ -1,9 +0,0 @@
-// models/ProcessingState.js
-import mongoose from "mongoose";
-
-const processingStateSchema = new mongoose.Schema({
-    key: { type: String, unique: true }, // e.g., "nse_filings"
-    lastProcessedTime: { type: Date }
-});
-
-export const ProcessingState = mongoose.model("ProcessingState", processingStateSchema);
